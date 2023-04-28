77ee452f1bd03da4410766e1e67fb62b50cc372a
JoDeMiro <joedemiro@pintye.com>
2023-04-28 13:28:25 +0000

    Add Color print out module

---
+++ cooler.py
@@ -0,0 +1,39 @@
+# deadline.py
+
+from termcolor import colored
+
+def printTest(value):
+
+	print(colored('---------------------------------------', 'yellow'))
+
+	print(colored(value, 'yellow'))
+
+	print(colored('---------------------------------------', 'yellow'))
+
+
+def printToDo(value):
+
+	print(colored('---------------------------------------', 'cyan'))
+
+	print(colored(value, 'cyan'))
+
+	print(colored('---------------------------------------', 'cyan'))
+
+def printColor(text, color):
+    
+	print(colored('---------------------------------------', color))
+
+	print(colored(text, color))
+
+	print(colored('---------------------------------------', color))
+
+
+def printBlink(text, color):
+    
+	print(colored('---------------------------------------', color))
+
+	print(colored(text, color, attrs=["reverse", "blink"]))
+
+	print(colored('---------------------------------------', color))
+
+# https://pypi.org/project/termcolor
